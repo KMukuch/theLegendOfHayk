8a16b70178113fb0bbfc612c485b9e1ed664e0fb
Mkrtich Kuloian <mkuloian@gmail.com>
2025-09-26 10:36:29 +0200

    game_map.json parsed. New struct Location_Connection created

--- include/game_map.h
+++ include/game_map.h
@@ -1,5 +1,5 @@
-#ifndef WORLD_H
-#define WORLD_H
+#ifndef GAME_MAP_H
+#define GAME_MAP_H
 
 #include <stdio.h>
 #include <cjson/cJSON.h>
@@ -9,14 +9,22 @@ struct Location {
     char location_name[MAXNAME];
     int game_map_size;
     Location_Type location_type;
-    struct Location **connections;
+    struct Location_Connection *connections;
     int connections_count;
 };
 
+struct Location_Connection {
+    const struct Location *location;
+    int connection_distance;
+    int connection_size;
+};
+
 struct Location create_location(const char *location_name, int size, Location_Type location_type);
+struct Location_Connection create_connection(const struct Location *location, int distance, int size);
+Location_Type parse_location_type(const char *type_str);
 cJSON* load_json_file(const char *filename);
 struct Location* init_game_map();
 // cJSON* find_nodes(cJSON *json);
-cJSON* find_connections(cJSON *json);
+// cJSON* find_connections(cJSON *json);
 
 #endif
--- src/game_map.c
+++ src/game_map.c
@@ -19,6 +19,25 @@ struct Location create_location(const char  *location_name, int size, Location_T
     return loc;
 }
 
+struct Location_Connection create_connection(const struct Location *location, int distance, int size)
+{
+    struct Location_Connection con;
+
+    con.location = location;
+    con.connection_distance = distance;
+    con.connection_size = size;
+
+    return con;
+}
+
+Location_Type parse_location_type(const char *type_str)
+{
+    if (strcmp(type_str, "REGION") == 0) return REGION;
+    if (strcmp(type_str, "CITY") == 0) return CITY;
+    if (strcmp(type_str, "VILLAGE") == 0) return VILLAGE;
+    return DEFAULT;
+}
+
 cJSON* load_json_file(const char *filename)
 {
     FILE *fp = fopen(filename, "rb");
@@ -70,7 +89,6 @@ struct Location* init_game_map()
 
     cJSON *json_file = load_json_file(FILENAME);
     // cJSON *json_nodes = find_nodes(json_file);
-    cJSON *json_connection = find_connections(json_file);
 
     game_map_size = cJSON_GetArraySize(json_file);
     // printf("%i\n", game_map_size);
@@ -83,16 +101,45 @@ struct Location* init_game_map()
 
     for (int i = 0; i < game_map_size; i++)
     {
-        cJSON *item = cJSON_GetArrayItem(json_file, i);
-        cJSON *name = cJSON_GetObjectItemCaseSensitive(item, "name");
-        if (cJSON_IsString(name) && name->valuestring != NULL) {
-            game_map[i] = create_location(name->valuestring, game_map_size, DEFAULT);
+        cJSON *json_item = cJSON_GetArrayItem(json_file, i);
+        cJSON *json_node = cJSON_GetObjectItemCaseSensitive(json_item, "node");
+        cJSON *json_type = cJSON_GetObjectItemCaseSensitive(json_item, "type");
+        if (cJSON_IsString(json_node) && json_node->valuestring != NULL && cJSON_IsString(json_type) && json_type->valuestring != NULL) 
+        {
+            game_map[i] = create_location(json_node->valuestring, game_map_size, parse_location_type(json_type->valuestring));
+        }
+    }
 
-            cJSON *connection = find_connections(item);
-            cJSON *connection_item = NULL;
-            cJSON_ArrayForEach(connection_item, connection)
+    for (int i = 0; i < game_map_size; i++)
+    {
+        int connection_size;
+        struct Location_Connection *connection;
+
+        cJSON *json_item = cJSON_GetArrayItem(json_file, i);
+        cJSON *json_connection = cJSON_GetObjectItemCaseSensitive(json_item, "connection");
+        connection_size = cJSON_GetArraySize(json_connection);
+        if (connection_size > 0)
+        {
+            connection = malloc(connection_size * sizeof(struct Location));
+            if (!connection)
             {
-                
+                fprintf(stderr, "Memory allocation failed\n");
+                cJSON_Delete(json_connection);
+                return NULL;
+            }
+            
+            for (int j = 0; j < connection_size; j++)
+            {
+                cJSON *json_connection_item = cJSON_GetArrayItem(json_connection, j);
+                cJSON *json_node = cJSON_GetObjectItemCaseSensitive(json_connection_item, "node");
+                cJSON *json_distance = cJSON_GetObjectItemCaseSensitive(json_connection_item, "distance");
+                if (cJSON_IsString(json_node) && json_node->valuestring != NULL && 0 == strcmp(game_map[i].location_name, json_node->valuestring)) 
+                {
+                    if (cJSON_IsNumber(json_distance)) 
+                    {
+                        connection[j] = create_connection(&game_map[i], json_distance->valueint, connection_size);
+                    }
+                }
             }
         }
     }
@@ -116,16 +163,16 @@ struct Location* init_game_map()
 //     return json_nodes;
 // }
 
-cJSON* find_connections(cJSON *json)
-{
-    cJSON *json_connection = NULL;
-    if (!json)
-    {
-        printf("Failed to load JSON file!\n");
-        return NULL;
-    }
+// cJSON* find_connections(cJSON *json)
+// {
+//     cJSON *json_connection = NULL;
+//     if (!json)
+//     {
+//         printf("Failed to load JSON file!\n");
+//         return NULL;
+//     }
     
-    json_connection = cJSON_GetObjectItemCaseSensitive(json, "connection");
+//     json_connection = cJSON_GetObjectItemCaseSensitive(json, "connection");
 
-    return json_connection;
-}
+//     return json_connection;
+// }
